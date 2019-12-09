c855b23e6ea284559bb2e18d14a3dbdcabc30b48
Peter Mulholland <pem42@pitt.edu>
2019-12-08 21:21:14 -0500

    fix ip, again

--- game.js
+++ game.js
@@ -87,7 +87,7 @@ var snek_right = new Image();
 var door = new Image();
 
 // WebRTC
-var signalingServer = "ws://18.233.98.225:8080";
+var signalingServer = "ws://74.98.229.161:8080";
 var peerConnection; // WebRTC connection to the controller
 var dataChannel; // Communicates with the controller
 var remoteDescriptionSet = false;
